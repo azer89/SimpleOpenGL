386ab9bf1ed2a4258ec36edfc0348aa2391ace1e
Reza Saputra <radhitya@uwaterloo.ca>
2023-10-12 17:52:05 -0400

    XML Reader work in progress

--- SimpleOpenGL/XMLReader.cpp
+++ SimpleOpenGL/XMLReader.cpp
@@ -1,1 +1,42 @@
 #include "XMLReader.h"
+#include "tinyxml2.h"
+
+#include <iostream>
+
+using namespace tinyxml2;
+
+/*unsigned int AppSettings::ScreenWidth = 0;
+unsigned int AppSettings::ScreenHeight = 0;
+std::string AppSettings::ScreenTitle = 0;
+std::string AppSettings::VertexShaderFile = "";
+std::string AppSettings::FragmentShaderFile = "";*/
+
+inline const char* GetString(const XMLElement* elem, const char* name)
+{
+	return elem->Attribute(name);
+}
+
+void XMLReader::LoadSettings()
+{
+	XMLDocument doc;
+	std::cout << "Parsing XML file: " << AppSettings::XMLFilePath << '\n';
+	auto eResult = doc.LoadFile(AppSettings::XMLFilePath);
+	if (eResult != XML_SUCCESS)
+	{
+		std::cerr << "Cannot find XML file: " << AppSettings::XMLFilePath << '\n';
+	}
+
+	// Root
+	auto root = doc.FirstChild();
+
+	// General settings
+	auto general_parent = root->FirstChildElement("General");
+	auto general_element = general_parent->FirstChildElement("material");
+}
+
+template<typename T>
+static T XMLReader::GetElementValue(const char* elementName)
+{
+
+}
+
--- SimpleOpenGL/XMLReader.h
+++ SimpleOpenGL/XMLReader.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef __APP_SETTINGS_H__
+#define __APP_SETTINGS_H__
+
+#include <iostream>
+
+namespace AppSettings
+{
+	const char* XMLFilePath = "C:/Users/azer/workspace/SimpleOpenGL/Settings/main.xml";
+
+	extern unsigned int ScreenWidth;
+	extern unsigned int ScreenHeight;
+	extern std::string ScreenTitle;
+	extern std::string VertexShaderFile;
+	extern std::string FragmentShaderFile;
+};
+
+static class XMLReader
+{
+private:
+	template<typename T>
+	static T GetElementValue(const char* elementName);
+
+public:
+	static void LoadSettings();
+};
+
+#endif
