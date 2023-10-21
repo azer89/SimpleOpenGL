aef076ac7e16b4c8c1aab71fb131f990c193ae09
Reza Saputra <radhitya@uwaterloo.ca>
2023-10-20 20:20:02 -0400

    camera and transformation

--- SimpleOpenGL/MainGL.cpp
+++ SimpleOpenGL/MainGL.cpp
@@ -4,11 +4,24 @@
 #include "AppSettings.h"
 #include "XMLReader.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 int MainGL::MainLoop()
 {
 	XMLReader::LoadSettings();
+
+	// Camera
+	camera = std::make_unique<Camera>(glm::vec3(0.0f, 0.0f, 3.0f));
+	lastX = AppSettings::ScreenWidth / 2.0f;
+	lastY = AppSettings::ScreenHeight / 2.0f;
+	firstMouse = true;
+	// Timing
+	deltaTime = 0.0f;	// Time between current frame and last frame
+	lastFrame = 0.0f;
 
 	// GLFW: initialize and configure
 	glfwInit();
@@ -31,11 +44,30 @@ int MainGL::MainLoop()
 	glfwMakeContextCurrent(glfwWindow);
 
 	glfwSetWindowUserPointer(glfwWindow, this);
-	auto func = [](GLFWwindow* window, int width, int height)
 	{
-		static_cast<MainGL*>(glfwGetWindowUserPointer(window))->FrameBufferSizeCallback(window, width, height);
-	};
-	glfwSetFramebufferSizeCallback(glfwWindow, func);
+		auto func = [](GLFWwindow* window, int width, int height)
+		{
+			static_cast<MainGL*>(glfwGetWindowUserPointer(window))->FrameBufferSizeCallback(window, width, height);
+		};
+		glfwSetFramebufferSizeCallback(glfwWindow, func);
+	}
+	{
+		auto func = [](GLFWwindow* window, double xpos, double ypos)
+		{
+			static_cast<MainGL*>(glfwGetWindowUserPointer(window))->MouseCallback(window, xpos, ypos);
+		};
+		glfwSetCursorPosCallback(glfwWindow, func);
+	}
+	{
+		auto func = [](GLFWwindow* window, double xoffset, double yoffset)
+		{
+			static_cast<MainGL*>(glfwGetWindowUserPointer(window))->ScrollCallback(window, xoffset, yoffset);
+		};
+		glfwSetScrollCallback(glfwWindow, func);
+	}
+
+	// Tell GLFW to capture our mouse
+	glfwSetInputMode(glfwWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -44,63 +76,144 @@ int MainGL::MainLoop()
 		return -1;
 	}
 
+	// Configure global opengl state
+	glEnable(GL_DEPTH_TEST);
+
+
 	// Shader programs
 	Shader shader(AppSettings::VertexShaderFile.c_str(), AppSettings::FragmentShaderFile.c_str());
 
 	// Texture
 	Texture texture(AppSettings::TextureFile.c_str(), GL_TEXTURE0);
 
-	float vertices[] = {
+	/*float vertices[] = {
 		// positions          // colors           // texture coords
 		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
 		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};*/
+
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-	unsigned int indices[] = {
+
+	// world space positions of our cubes
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	
+	/*unsigned int indices[] = {
 		0, 1, 3, // first triangle
 		1, 2, 3  // second triangle
-	};
-	unsigned int VBO, VAO, EBO;
+	};*/
+	unsigned int VBO, VAO; //EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
 
 	// Texture coord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// Render loop
 	while (!glfwWindowShouldClose(glfwWindow))
 	{
+		// Per-frame time
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// Input
 		ProcessInput(glfwWindow);
 
 		// Render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		texture.Bind();
 
 		shader.Use();
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		 // pass projection matrix to shader (note that in this case it could change every frame)
+		glm::mat4 projection = glm::perspective(glm::radians(camera->Zoom), (float)AppSettings::ScreenWidth / (float)AppSettings::ScreenHeight, 0.1f, 100.0f);
+		shader.SetMat4("projection", projection);
+		// camera/view transformation
+		glm::mat4 view = camera->GetViewMatrix();
+		shader.SetMat4("view", view);
+		// render boxes
+		glBindVertexArray(VAO);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader.SetMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// GLFW: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(glfwWindow);
@@ -110,7 +223,7 @@ int MainGL::MainLoop()
 	// De-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	//glDeleteBuffers(1, &EBO);
 	shader.Delete();
 
 	// GLFW: terminate, clearing all previously allocated GLFW resources.
@@ -126,11 +239,53 @@ void MainGL::FrameBufferSizeCallback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+void MainGL::MouseCallback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+	camera->ProcessMouseMovement(xoffset, yoffset);
+}
+
+void MainGL::ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera->ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
 // Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 void MainGL::ProcessInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, true);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		camera->ProcessKeyboard(CameraForward, deltaTime);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera->ProcessKeyboard(CameraBackward, deltaTime);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera->ProcessKeyboard(CameraLeft, deltaTime);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera->ProcessKeyboard(CameraRight, deltaTime);
 	}
 }
--- SimpleOpenGL/MainGL.h
+++ SimpleOpenGL/MainGL.h
@@ -1,9 +1,12 @@
 #ifndef __MAIN_GL_H__
 #define __MAIN_GL_H__
 
+#include <memory>
 #include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "Camera.h"
 
 class MainGL
 {
@@ -12,7 +15,22 @@ public:
 
 private:
 	void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
+	void MouseCallback(GLFWwindow* window, double xpos, double ypos);
+	void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
 	void ProcessInput(GLFWwindow* window);
+
+private:
+	// Camera
+	std::unique_ptr<Camera> camera;
+	float lastX;
+	float lastY;
+	bool firstMouse;
+
+	// Timing
+	float deltaTime; // Time between current frame and last frame
+	float lastFrame;
+
 };
 
 #endif
