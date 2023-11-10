61da2005a95bf44f25af3bbe845c5abdb21ef2ab
Reza Saputra <radhitya@uwaterloo.ca>
2023-11-10 16:57:11 -0500

    g buffer class usage

--- SimpleOpenGL/AppDeferred.cpp
+++ SimpleOpenGL/AppDeferred.cpp
@@ -1,6 +1,7 @@
 #include "AppDeferred.h"
 #include "AppSettings.h"
 #include "ShapeFactory.h"
+#include "GBuffer.h"
 
 #include <memory>
 
@@ -13,64 +14,12 @@ int AppDeferred::MainLoop()
 
 	glEnable(GL_DEPTH_TEST);
 
-	Shader gBufferShader("deferred_g_buffer.vertex", "deferred_g_buffer.fragment");
-	Shader lightingShader("deferred_lighting.vertex", "deferred_lighting.fragment");
-	lightingShader.Use();
-	lightingShader.SetInt("gPosition", 0);
-	lightingShader.SetInt("gNormal", 1);
-	lightingShader.SetInt("gAlbedoSpec", 2);
-
 	Shader lightSphereShader("light_sphere.vertex", "light_sphere.fragment");
 
 	Texture grassTexture;
 	grassTexture.CreateFromImageFile(AppSettings::TextureFolder + "grass.png");
 
-	// G buffer
-	unsigned int gBuffer;
-	unsigned int gPosition, gNormal, gAlbedoSpec;
-	glGenFramebuffers(1, &gBuffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
-	
-	// Position
-	glGenTextures(1, &gPosition);
-	glBindTexture(GL_TEXTURE_2D, gPosition);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, AppSettings::ScreenWidth, AppSettings::ScreenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPosition, 0);
-	
-	// Normal
-	glGenTextures(1, &gNormal);
-	glBindTexture(GL_TEXTURE_2D, gNormal);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, AppSettings::ScreenWidth, AppSettings::ScreenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormal, 0);
-	
-	// Color + Specular
-	glGenTextures(1, &gAlbedoSpec);
-	glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, AppSettings::ScreenWidth, AppSettings::ScreenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, gAlbedoSpec, 0);
-	
-	unsigned int attachments[3] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
-	glDrawBuffers(3, attachments);
-
-	// Render buffer
-	unsigned int rboDepth;
-	glGenRenderbuffers(1, &rboDepth);
-	glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, AppSettings::ScreenWidth, AppSettings::ScreenHeight);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
-	
-	// Check if framebuffer is complete
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-	{
-		std::cerr << "Framebuffer not complete!" << '\n';
-	}
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	GBuffer gBuffer;
 
 	InitLights();
 	InitScene();
@@ -86,52 +35,23 @@ int AppDeferred::MainLoop()
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// 1 Geometry pass
-		glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glm::mat4 projection = camera->GetProjectionMatrix();
 		glm::mat4 view = camera->GetViewMatrix();
-		glm::mat4 model = glm::mat4(1.0f);
-		gBufferShader.Use();
-		gBufferShader.SetMat4("projection", projection);
-		gBufferShader.SetMat4("view", view);
+
+
+		// 1 Geometry pass
+		gBuffer.StartGeometryPass(projection, view);
+		Shader* geomShaderPtr = gBuffer.GetGeometryShader();
 		grassTexture.Bind(GL_TEXTURE0);
-		RenderPlane(gBufferShader);
-		RenderFoxes(gBufferShader);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		RenderPlane(*geomShaderPtr);
+		RenderFoxes(*geomShaderPtr);
+		gBuffer.EndGeometryPass();
 
 		// 2 Lighting Pass
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		lightingShader.Use();
-		lightingShader.SetVec3("viewPos", camera->Position);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, gPosition);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, gNormal);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
-		// Send light relevant uniforms
-		const float linear = 0.7f;
-		const float quadratic = 1.8f;
-		for (unsigned int i = 0; i < lights.size(); i++)
-		{
-			lightingShader.SetVec3("lights[" + std::to_string(i) + "].Position", lights[i].Position);
-			lightingShader.SetVec3("lights[" + std::to_string(i) + "].Color", lights[i].Color);
-			// Update attenuation parameters and calculate radius
-			lightingShader.SetFloat("lights[" + std::to_string(i) + "].Linear", linear);
-			lightingShader.SetFloat("lights[" + std::to_string(i) + "].Quadratic", quadratic);
-		}
-		// Render quad
-		RenderQuad();
+		gBuffer.StartLightingPass(lights, camera->Position);
 
 		// 3 Copy content of geometry's depth buffer to default framebuffer's depth buffer
-		glBindFramebuffer(GL_READ_FRAMEBUFFER, gBuffer);
-		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0); // write to default framebuffer
-		// Blit to default framebuffer. Note that this may or may not work as the internal formats of both the FBO and default framebuffer have to match.
-		// the internal formats are implementation defined. This works on all of my systems, but if it doesn't on yours you'll likely have to write to the 		
-		// depth buffer in another shader stage (or somehow see to match the default framebuffer's internal format with the FBO's internal format).
-		glBlitFramebuffer(0, 0, AppSettings::ScreenWidth, AppSettings::ScreenHeight, 0, 0, AppSettings::ScreenWidth, AppSettings::ScreenHeight, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		gBuffer.Blit();
 
 		// Render lights on top of scene
 		lightSphereShader.Use();
@@ -230,31 +150,4 @@ void AppDeferred::RenderFoxes(const Shader& shader)
 	model = glm::scale(model, glm::vec3(0.03f));
 	shader.SetMat4("model", model);
 	foxModel->Draw(shader);
-}
-
-void AppDeferred::RenderQuad()
-{
-	if (quadVAO == 0)
-	{
-		float quadVertices[] = {
-			// Positions		// Texture Coords
-			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
-			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-		};
-
-		glGenVertexArrays(1, &quadVAO);
-		glGenBuffers(1, &quadVBO);
-		glBindVertexArray(quadVAO);
-		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	}
-	glBindVertexArray(quadVAO);
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	glBindVertexArray(0);
 }
--- SimpleOpenGL/AppDeferred.h
+++ SimpleOpenGL/AppDeferred.h
@@ -14,7 +14,6 @@ public:
 private:
 	void InitScene();
 	void InitLights(); 
-	//void InitLightCube();
 
 	void RenderPlane(const Shader& shader);
 	void RenderFoxes(const Shader& shader);
@@ -24,18 +23,10 @@ private:
 private:
 	std::unique_ptr<Model> foxModel;
 
-	//std::vector<glm::vec3> lightPositions;
-	//std::vector<glm::vec3> lightColors;
 	std::vector<Light> lights;
-
-	//unsigned int lightCubeVAO = 0;
-	//unsigned int lightCubeVBO = 0;
 
 	unsigned int planeVAO = 0;
 	unsigned int planeVBO = 0;
-
-	unsigned int quadVAO = 0;
-	unsigned int quadVBO = 0;
 };
 
 
