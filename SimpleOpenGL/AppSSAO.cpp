981509677c28556dff0e93c0a9bd2c0216624548
Reza Saputra <radhitya@uwaterloo.ca>
2023-11-12 06:26:50 -0500

    wip SSAO

---
+++ SimpleOpenGL/AppSSAO.cpp
@@ -0,0 +1,27 @@
+#include "AppSSAO.h"
+
+int AppSSAO::MainLoop()
+{
+	if (!IsGLFWWindowCreated() || !IsGLADLoaded())
+	{
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	while (!GLFWWindowShouldClose())
+	{
+		ProcessTiming();
+		ProcessInput();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		SwapBuffers();
+		PollEvents();
+	}
+
+	Terminate();
+
+	return 0;
+}
---
+++ SimpleOpenGL/AppSSAO.h
@@ -0,0 +1,12 @@
+#ifndef __APP_SAAO_H_
+#define __APP_SAAO_H_
+
+#include "AppBase.h"
+
+class AppSSAO : AppBase
+{
+public:
+	int MainLoop() override;
+};
+
+#endif
