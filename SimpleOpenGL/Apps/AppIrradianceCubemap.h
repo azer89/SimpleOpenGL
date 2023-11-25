ac6444a65fcaef5a805f3e7f2a33bc379b38cf38
Reza Saputra <radhitya@uwaterloo.ca>
2023-11-25 05:33:40 -0500

    hdr

---
+++ SimpleOpenGL/Apps/AppIrradianceCubemap.h
@@ -0,0 +1,12 @@
+#ifndef __IRRADIANCE_CUBEMAP_H__
+#define __IRRADIANCE_CUBEMAP_H__
+
+#include "AppBase.h"
+
+class AppIrradianceCubemap : AppBase
+{
+public:
+	int MainLoop() override;
+};
+
+#endif
