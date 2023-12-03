51b3fd579b0f4f9ab6e69f348b3489006403ed65
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-03 13:03:47 -0500

    upgraded glad to opengl 4.6

--- SimpleOpenGL/Apps/AppBase.cpp
+++ SimpleOpenGL/Apps/AppBase.cpp
@@ -20,8 +20,8 @@ void AppBase::InitGLFW()
 
 	// GLFW: initialize and configure
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// GLFW window creation
