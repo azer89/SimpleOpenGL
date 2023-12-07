fb2dd2f9d696ef679983116c399d579e77eb3755
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-07 06:07:17 -0500

    Deferred + SSAO

---
+++ SimpleOpenGL/Apps/AppDeferredSSAO.cpp
@@ -0,0 +1,162 @@
+#include "AppDeferredSSAO.h"
+#include "AppSettings.h"
+#include "Utility.h"
+#include "PipelineDeferredSSAO.h"
+
+#include <random>
+
+int AppDeferredSSAO::MainLoop()
+{
+	glEnable(GL_DEPTH_TEST);
+
+	InitScene();
+	InitLights();
+
+	int noiseSize = 4;
+	int kernelSize = 64;
+	float radius = 0.5f;
+	float bias = 0.025f;
+
+	PipelineDeferredSSAO pipeline(
+		"SSAO//geometry.vertex", "SSAO//geometry.fragment",
+		"SSAO//ssao.vertex", "SSAO//lighting.fragment",
+		"SSAO//ssao.vertex", "SSAO//ssao.fragment",
+		"SSAO//ssao.vertex", "SSAO//blur.fragment",
+		kernelSize,
+		noiseSize
+	);
+
+	// Game loop
+	while (!GLFWWindowShouldClose())
+	{
+		ProcessLoop(
+			glm::vec4(0.2f, 0.3f, 0.3f, 1.0f),
+			GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
+		);
+
+		glm::mat4 projection = camera->GetProjectionMatrix();
+		glm::mat4 view = camera->GetViewMatrix();
+
+		// 1 Geometry pass: render scene's geometry/color data into G buffer
+		pipeline.StartGeometryPass(projection, view);
+		RenderScene(*(pipeline.GetGeometryShader()));
+		pipeline.EndGeometryPass();
+
+		// 2 SSAO
+		pipeline.StartSSAOPass(projection, kernelSize, radius, bias);
+
+		// 3 Blur SSAO texture to remove noise
+		pipeline.StartBlurPass();
+
+		//UpdateLightPositions();
+
+		// 4 lighting pass: traditional deferred Blinn-Phong lighting with added screen-space ambient occlusion
+		pipeline.StartLightingPass(lights, view, camera->Position);
+
+		// 5 Blit
+		pipeline.Blit();
+
+		RenderLights();
+
+		if (showImgui)
+		{
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			ImGui::SetNextWindowSize(ImVec2(500, 100));
+
+			ImGui::Begin("SSAO");
+
+			ImGui::SliderFloat("Radius", &radius, 0.0f, 50.0f);
+			ImGui::SliderFloat("Bias", &bias, 0.0f, 0.5f);
+
+			ImGui::End();
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		}
+
+		SwapBuffers();
+	}
+
+	Terminate();
+
+	return 0;
+}
+
+void AppDeferredSSAO::InitScene()
+{
+	sponzaModel = std::make_unique<Model>(AppSettings::ModelFolder + "Sponza//Sponza.gltf");
+}
+
+void AppDeferredSSAO::RenderScene(const Shader& shader)
+{
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(8.0f, 0.0f, 1.0f));
+	model = glm::scale(model, glm::vec3(2.0f));
+	shader.SetMat4("model", model);
+	sponzaModel->Draw(shader);
+}
+
+void AppDeferredSSAO::InitLights()
+{
+	lightSphereShader = std::make_unique<Shader>("Misc//light_sphere.vertex", "Misc//light_sphere.fragment");
+
+	float pi2 = glm::two_pi<float>();
+
+	const int NR_LIGHTS = 200;
+	for (unsigned int i = 0; i < NR_LIGHTS; ++i)
+	{
+		float yPos = Utility::RandomNumber<float>(0.15f, 10.0f);
+		float radius = Utility::RandomNumber<float>(5.0f, 15.0f);
+		float rad = Utility::RandomNumber<float>(0.0f, pi2);
+		float xPos = glm::cos(rad);
+
+		glm::vec3 position(
+			glm::cos(rad) * radius,
+			yPos,
+			glm::sin(rad) * radius
+		);
+
+		glm::vec3 color(
+			Utility::RandomNumber<float>(0.5f, 0.8f),
+			Utility::RandomNumber<float>(0.5f, 0.8f),
+			Utility::RandomNumber<float>(0.7f, 1.0f)
+		);
+
+		lightAngles.push_back(rad);
+		lightRadii.push_back(radius);
+		lights.emplace_back(position, color, true, 0.1f);
+	}
+
+	lightSphereShader->Use();
+	lightSphereShader->SetFloat("radius", 0.2f);
+}
+
+void AppDeferredSSAO::RenderLights()
+{
+	lightSphereShader->Use();
+	lightSphereShader->SetMat4("projection", camera->GetProjectionMatrix());
+	lightSphereShader->SetMat4("view", camera->GetViewMatrix());
+	for (Light& light : lights)
+	{
+		light.Render(*lightSphereShader);
+	}
+}
+
+void AppDeferredSSAO::UpdateLightPositions()
+{
+	for (unsigned int i = 0; i < lights.size(); ++i)
+	{
+		float step = deltaTime * 0.2f;
+
+		float yPos = lights[i].Position.y;
+		lightAngles[i] += step;
+
+		lights[i].Position = glm::vec3(
+			glm::cos(lightAngles[i]) * lightRadii[i],
+			yPos,
+			glm::sin(lightAngles[i]) * lightRadii[i]
+		);
+	}
+}
---
+++ SimpleOpenGL/Apps/AppDeferredSSAO.h
@@ -0,0 +1,35 @@
+#ifndef __APP_SAAO_H_
+#define __APP_SAAO_H_
+
+#include "AppBase.h"
+#include "Model.h"
+#include "Texture.h"
+#include "Light.h"
+
+#include <memory>
+
+class AppDeferredSSAO : AppBase
+{
+public:
+	int MainLoop() override;
+
+private:
+	void InitScene();
+	void RenderScene(const Shader& shader);
+
+	void InitLights();
+	void RenderLights();
+	void UpdateLightPositions();
+
+private:
+	// Scene
+	std::unique_ptr<Model> sponzaModel;
+
+	// Lights
+	std::unique_ptr<Shader> lightSphereShader;
+	std::vector<Light> lights;
+	std::vector<float> lightAngles;
+	std::vector<float> lightRadii;
+};
+
+#endif
