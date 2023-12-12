53731bcd80a417720f4f9ba7686f3d4309c2d00b
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-12 05:16:50 -0500

    shadow mapping pipeline

---
+++ SimpleOpenGL/System/PipelineShadowMapping.cpp
@@ -0,0 +1,99 @@
+#include "PipelineShadowMapping.h"
+#include "AppSettings.h"
+
+#include "glm/glm.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+constexpr unsigned int DEPTH_WIDTH = 2000;
+constexpr unsigned int DEPTH_HEIGHT = 2000;
+
+PipelineShadowMapping::PipelineShadowMapping()
+{
+	Init();
+}
+
+void PipelineShadowMapping::Init()
+{
+	mainShader = std::make_unique<Shader>("ShadowMapping//shadow_mapping.vertex", "ShadowMapping//shadow_mapping.fragment");
+	depthShader = std::make_unique<Shader>("ShadowMapping//shadow_mapping_depth.vertex", "ShadowMapping//shadow_mapping_depth.fragment");
+	debugShader = std::make_unique<Shader>("ShadowMapping//shadow_mapping_debug.vertex", "ShadowMapping//shadow_mapping_debug.fragment");
+
+	mainShader->Use();
+	mainShader->SetInt("diffuseTexture", 0);
+	mainShader->SetInt("shadowMap", 1);
+	debugShader->Use();
+	debugShader->SetInt("depthMap", 0);
+
+	// Depth
+	depthTexture = std::make_unique<Texture>();
+	depthTexture->CreateDepthMap(DEPTH_WIDTH, DEPTH_HEIGHT);
+
+	// FBO
+	glCreateFramebuffers(1, &depthFBO);
+	glNamedFramebufferTexture(depthFBO, GL_DEPTH_ATTACHMENT, depthTexture->GetID(), 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void PipelineShadowMapping::SetInfo(ShadowInfo info)
+{
+	currInfo = info;
+
+	glm::mat4 lightProjection =
+		glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, currInfo.shadowNearPlane, currInfo.shadowFarPlane);
+	glm::mat4 lightView =
+		glm::lookAt(currInfo.lightPosition, currInfo.target, glm::vec3(0.0, 1.0, 0.0));
+	lightSpaceMatrix = lightProjection * lightView;
+}
+
+void PipelineShadowMapping::StartRenderDepth()
+{
+	// Render depth
+	depthShader->Use();
+	depthShader->SetMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+	glViewport(0, 0, DEPTH_WIDTH, DEPTH_HEIGHT);
+	glBindFramebuffer(GL_FRAMEBUFFER, depthFBO);
+	glClear(GL_DEPTH_BUFFER_BIT);
+}
+
+void PipelineShadowMapping::SopRenderDepth()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glViewport(0, 0, AppSettings::ScreenWidth, AppSettings::ScreenHeight);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+/*
+struct ShadowInfo
+{
+	glm::vec3 lightPosition;
+	glm::vec3 target;
+
+	float minBias;
+	float maxBias;
+	float shadowNearPlane;
+	float shadowFarPlane;
+
+	glm::mat4 cameraProjection;
+	glm::mat4 cameraView;
+	glm::mat4 cameraPosition;
+
+	float ambientPower;
+	int specularPower;
+};
+*/
+
+void PipelineShadowMapping::SetMainShader()
+{
+	// Render Scene
+	mainShader->Use();
+	mainShader->SetFloat("minBias", currInfo.minBias);
+	mainShader->SetFloat("maxBias", currInfo.maxBias);
+	mainShader->SetFloat("ambientPower", currInfo.ambientPower);
+	mainShader->SetInt("specularPower", currInfo.specularPower);
+	mainShader->SetMat4("projection", currInfo.cameraProjection);
+	mainShader->SetMat4("view", currInfo.cameraView);
+	mainShader->SetVec3("viewPos", currInfo.cameraPosition);
+	mainShader->SetVec3("lightPos", currInfo.lightPosition);
+	mainShader->SetMat4("lightSpaceMatrix", lightSpaceMatrix);
+}
---
+++ SimpleOpenGL/System/PipelineShadowMapping.h
@@ -0,0 +1,79 @@
+#ifndef __PIPELINE_SHADOW_MAPPING_H__
+#define __PIPELINE_SHADOW_MAPPING_H__
+
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+
+#include <memory>
+
+/*glm::vec3 lightPos;
+constexpr float lightY = 6.0f;
+constexpr float lightRadius = 5.0f;
+constexpr float lightSpeed = 0.5f;
+float lightTimer = 0.0f;*/
+
+/*
+// Render Scene
+		mainShader.Use();
+		mainShader.SetFloat("minBias", minBias);
+		mainShader.SetFloat("maxBias", maxBias);
+		mainShader.SetFloat("ambientPower", ambientPower); //
+		mainShader.SetInt("specularPower", specularPower); //
+		mainShader.SetMat4("projection", camera->GetProjectionMatrix());
+		mainShader.SetMat4("view", camera->GetViewMatrix());
+		mainShader.SetVec3("viewPos", camera->Position);
+		mainShader.SetVec3("lightPos", lightPos);
+		mainShader.SetMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+*/
+
+struct ShadowInfo
+{
+	glm::vec3 lightPosition;
+	glm::vec3 target;
+
+	float minBias;
+	float maxBias;
+	float shadowNearPlane;
+	float shadowFarPlane;
+
+	glm::mat4 cameraProjection;
+	glm::mat4 cameraView;
+	glm::vec3 cameraPosition;
+
+	float ambientPower;
+	int specularPower;
+};
+
+class PipelineShadowMapping
+{
+public:
+	PipelineShadowMapping();
+
+	Shader* GetMainShader() { return mainShader.get(); }
+
+private:
+	void Init();
+
+	void SetInfo(ShadowInfo info);
+	void StartRenderDepth();
+	void SopRenderDepth();
+
+	void SetMainShader();
+
+private:
+	// Data
+	ShadowInfo currInfo;
+	glm::mat4 lightSpaceMatrix;
+
+	std::unique_ptr<Shader> mainShader;
+	std::unique_ptr<Shader> depthShader;
+	std::unique_ptr<Shader> debugShader;
+
+	std::unique_ptr<Texture> depthTexture;
+	unsigned int depthFBO;
+};
+
+#endif
