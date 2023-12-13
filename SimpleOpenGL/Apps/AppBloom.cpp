38362c58f87647cf220f0f0e5aeb659245fb6191
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-13 17:45:08 -0500

    bloom quad

--- SimpleOpenGL/Apps/AppBloom.cpp
+++ SimpleOpenGL/Apps/AppBloom.cpp
@@ -177,24 +177,30 @@ void RenderQuad()
 {
 	if (quadVAO == 0)
 	{
-		float quadVertices[] = {
-			// positions        // texture Coords
+		// Quad
+		constexpr float quadVertices[] = {
+			// Positions		// Texture Coords
 			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
 			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
 			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
 			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
 		};
-		// setup plane VAO
-		glGenVertexArrays(1, &quadVAO);
-		glGenBuffers(1, &quadVBO);
-		glBindVertexArray(quadVAO);
-		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+		glCreateBuffers(1, &quadVBO);
+		glNamedBufferStorage(quadVBO, sizeof(quadVertices), &quadVertices, GL_DYNAMIC_STORAGE_BIT);
+		glCreateVertexArrays(1, &quadVAO);
+		glVertexArrayVertexBuffer(quadVAO, 0, quadVBO, 0, 5 * sizeof(float));
+
+		glEnableVertexArrayAttrib(quadVAO, 0);
+		glEnableVertexArrayAttrib(quadVAO, 1);
+
+		glVertexArrayAttribFormat(quadVAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribFormat(quadVAO, 1, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
+
+		glVertexArrayAttribBinding(quadVAO, 0, 0);
+		glVertexArrayAttribBinding(quadVAO, 1, 0);
 	}
+
 	glBindVertexArray(quadVAO);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	glBindVertexArray(0);
