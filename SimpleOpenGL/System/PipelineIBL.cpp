082d9aa18f969fce2ac6585bd9db174313bb0330
Reza Saputra <radhitya@uwaterloo.ca>
2024-02-11 07:32:39 -0500

    const

--- SimpleOpenGL/System/PipelineIBL.cpp
+++ SimpleOpenGL/System/PipelineIBL.cpp
@@ -36,9 +36,9 @@ void PipelineIBL::Init(
 	Cube cube;
 
 	// Quad
-	float quadRotation = acos(0);
-	glm::vec3 quadRotationAxis(1.f, 0.f, 0.f);
-	Quad quad(quadRotation, quadRotationAxis);
+	const float quadRotation = acos(0);
+	const glm::vec3 quadRotationAxis(1.f, 0.f, 0.f);
+	const Quad quad(quadRotation, quadRotationAxis);
 
 	pbrShader = std::make_unique<Shader>("IBL//pbr.vertex", pbrShaderFile.c_str());
 	Shader equirectangularToCubemapShader("IBL//cubemap.vertex", "IBL//equirectangular_to_cubemap.fragment");
@@ -67,7 +67,7 @@ void PipelineIBL::Init(
 	hdrTexture.CreateFromHDRFile(AppSettings::TextureFolder + hdrFile);
 
 	// PBR set up projection and view matrices for capturing data onto the 6 cubemap face directions
-	glm::mat4 captureProjection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 10.0f);
+	const glm::mat4 captureProjection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 10.0f);
 	std::vector<glm::mat4> captureViews =
 	{
 		glm::lookAt(glm::vec3(0.0f), glm::vec3(1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
@@ -169,7 +169,7 @@ void PipelineIBL::Init(
 		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, mipWidth, mipHeight);
 		glViewport(0, 0, mipWidth, mipHeight);
 
-		float roughness = (float)mip / (float)(maxMipLevels - 1);
+		const float roughness = (float)mip / (float)(maxMipLevels - 1);
 		prefilterShader.SetFloat("roughness", roughness);
 		for (unsigned int i = 0; i < captureViews.size(); ++i)
 		{
