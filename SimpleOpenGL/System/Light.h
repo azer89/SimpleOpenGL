efec24ed1f721bd99ae3a712027e58355ef5c2d4
Reza Saputra <radhitya@uwaterloo.ca>
2024-06-27 08:00:10 -0400

    Light

--- SimpleOpenGL/System/Light.h
+++ SimpleOpenGL/System/Light.h
@@ -11,18 +11,18 @@
 class Light
 {
 public:
-	glm::vec3 Position;
-	glm::vec3 Color;
-
-public:
 	Light(glm::vec3 position, glm::vec3 color, bool debugShow = true, float debugRadius = 0.1f);
 	void Render(const Shader& shader) const;
 
-private:
-	bool shouldRender;
-	float renderRadius;
+public:
+	glm::vec3 Position{};
+	glm::vec3 Color{};
 
-	std::unique_ptr<Quad> quad;
+private:
+	bool shouldRender{};
+	float renderRadius{};
+
+	std::unique_ptr<Quad> quad{};
 };
 
 #endif
